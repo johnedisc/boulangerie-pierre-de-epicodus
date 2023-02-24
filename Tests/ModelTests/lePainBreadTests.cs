f2e4da35fa0ebc5194f1c75d7bc3dd866a6fcb58
johnedisc <christopher.johnedis@gmail.com>
2023-02-24 11:27:58 -0800

    fail 2nd test

--- Tests/ModelTests/lePainBreadTests.cs
+++ Tests/ModelTests/lePainBreadTests.cs
@@ -8,18 +8,26 @@ namespace Boulangerie.Tests
   {
 
     [TestMethod]
-    public void BreadConstructor_MakesInstanceOf_()
+    public void BreadConstructor_MakesInstanceOfBread_Bread()
     {
       Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
     [TestMethod]
-    public void BreadConstructor_ReturnsNumberOfBreadsWanted_()
+    public void BreadConstructor_ReturnsNumberOfBreadsWanted_Int()
     {
       Bread newBread = new Bread(1);
       int test = 1;
       Assert.AreEqual(test, newBread.OrderQuantity);
+    }
+
+    [TestMethod]
+    public void CalculateBreadCost_ReturnsBreadCostWithDiscount_Int()
+    {
+      Bread newBread = new Bread(7);
+      int expectedResult = 25;
+      Assert.AreEqual(expectedResult, newBread.CalculateBreadCost());
     }
 
   }
