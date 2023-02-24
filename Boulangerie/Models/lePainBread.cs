2b1fe2c8c0b572069f56548be7f4f35053cd0bc9
johnedisc <christopher.johnedis@gmail.com>
2023-02-24 11:15:27 -0800

    passed 2nd test, returns quantity

--- Boulangerie/Models/lePainBread.cs
+++ Boulangerie/Models/lePainBread.cs
@@ -4,6 +4,16 @@ namespace Boulangerie.Models
 {
   public class Bread
   {
+    private int _orderQuantity;
+    public int OrderQuantity
+    {
+      get { return _orderQuantity; }
+      set { _orderQuantity = value; }
+    }
 
+    public Bread(int clientOrder)
+    {
+      _orderQuantity = clientOrder;
+    }
   }
 }
