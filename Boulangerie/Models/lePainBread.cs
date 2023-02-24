03a37ba0c901b27ec0b1925c0a0feb04e7dd2fda
johnedisc <christopher.johnedis@gmail.com>
2023-02-24 11:57:16 -0800

    3rd test passed, calculate bread cost returns expected value

--- Boulangerie/Models/lePainBread.cs
+++ Boulangerie/Models/lePainBread.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace Boulangerie.Models
 {
@@ -10,10 +11,23 @@ namespace Boulangerie.Models
       get { return _orderQuantity; }
       set { _orderQuantity = value; }
     }
+    private int _breadUnitCost = 5;
+    public int BreadUnitCost
+    {
+      get { return _breadUnitCost; }
+      set { _breadUnitCost = value; }
+    }
 
     public Bread(int clientOrder)
     {
       _orderQuantity = clientOrder;
+    }
+
+    public int CalculateBreadCost()
+    {
+      int groupsOf3 = _orderQuantity / 3;
+      int totalBreadCost = _breadUnitCost * ((2 * groupsOf3) + (_orderQuantity-(groupsOf3) * 3));
+      return totalBreadCost;
     }
   }
 }
