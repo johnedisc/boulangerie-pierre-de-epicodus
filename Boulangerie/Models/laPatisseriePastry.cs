a2efde196ebd755eb70af4dba6cdc66657fb9e49
johnedisc <christopher.johnedis@gmail.com>
2023-02-24 12:07:45 -0800

    postive fail first pastry test

--- Boulangerie/Models/laPatisseriePastry.cs
+++ Boulangerie/Models/laPatisseriePastry.cs
@@ -2,9 +2,33 @@ using System.Collections.Generic;
 
 namespace Boulangerie.Models
 {
-  public class LaPatisseriePastry
+  public class Pastry
   {
 
+//    private int _orderQuantity;
+//    public int OrderQuantity
+//    {
+//      get { return _orderQuantity; }
+//      set { _orderQuantity = value; }
+//    }
+//    private int _breadUnitCost = 5;
+//    public int BreadUnitCost
+//    {
+//      get { return _breadUnitCost; }
+//      set { _breadUnitCost = value; }
+//    }
+//
+//    public Bread(int clientOrder)
+//    {
+//      _orderQuantity = clientOrder;
+//    }
+//
+//    public int CalculateBreadCost()
+//    {
+//      int groupsOf3 = _orderQuantity / 3;
+//      int totalBreadCost = _breadUnitCost * ((2 * groupsOf3) + (_orderQuantity-(groupsOf3) * 3));
+//      return totalBreadCost;
+//    }
 
   }
 }
--- Tests/ModelTests/laPatisseriePastryTests.cs
+++ Tests/ModelTests/laPatisseriePastryTests.cs
@@ -3,7 +3,35 @@ using Boulangerie.Models;
 using System.Collections.Generic;
 using System;
 
-namespace LaPatisseriePastry.Tests
+namespace Boulangerie.Tests
 {
+  [TestClass]
+  public class PastryTests
+  {
+
+    [TestMethod]
+    public void PastryConstructor_MakesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+//    [TestMethod]
+//    public void PastryConstructor_ReturnsNumberOfPastrysWanted_Int()
+//    {
+//      Pastry newPastry = new Pastry(1);
+//      int test = 1;
+//      Assert.AreEqual(test, newPastry.OrderQuantity);
+//    }
+//
+//    [TestMethod]
+//    public void CalculatePastryCost_ReturnsPastryCostWithDiscount_Int()
+//    {
+//      Pastry newPastry = new Pastry(7);
+//      int expectedResult = 25;
+//      Assert.AreEqual(expectedResult, newPastry.CalculatePastryCost());
+//    }
+
+  }
 }
 
