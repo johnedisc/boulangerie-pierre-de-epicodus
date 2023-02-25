a5577f7e02e02bc144c3c16624f0745cae102ddb
johnedisc <christopher.johnedis@gmail.com>
2023-02-24 16:51:48 -0800

    complete validate integer method and add order method to take order with error handling

--- Boulangerie/Program.cs
+++ Boulangerie/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Collections.Generic;
 using Boulangerie.Models;
 
 namespace Boulangerie
@@ -20,7 +21,7 @@ namespace Boulangerie
     {
       Console.WriteLine("pour le français entrez 1");
       Console.WriteLine("for english enter 2");
-      int langSelect = ValidateInteger(Console.ReadLine(), "langHandler");
+      int langSelect = ValidateInteger(Console.ReadLine());
       if (langSelect == 1)
       {
         French();
@@ -36,23 +37,24 @@ namespace Boulangerie
       }
     }
 
-    static bool ValidateInteger(string usrValue,string originalFunc)
+    static int ValidateInteger(string usrValue)
     {
       try
       {
-        int test = Int32.Parse(usrValue);
-        return true;
+        return Int32.Parse(usrValue);
       }
       catch (FormatException ex)
       {
-        Console.WriteLine("{0} please enter an integer", ex.Message);
+        Console.WriteLine("{0} please enter an integer.", ex.Message.ToLower());
+        return 0;
       }
     }
 
     static void French()
     {
-          Console.WriteLine("francais");
-          originalFunc();
+      Console.WriteLine("today's pastries are: ${0}. buy 3, get one free.", Pastry.PastryUnitCost);
+      Console.WriteLine("and bread is: ${0}. buy 2, get one free.", Bread.BreadUnitCost);
+      List<int> order = new List<int>(takeOrder("en"));
     }
     
     static void English()
@@ -63,11 +65,32 @@ namespace Boulangerie
           Console.WriteLine("welcome to generic uninteresting American pastries.");
           Console.WriteLine("today's pastries are: ${0}. buy 3, get one free.", Pastry.PastryUnitCost);
           Console.WriteLine("and bread is: ${0}. buy 2, get one free.", Bread.BreadUnitCost);
-          Console.WriteLine("how many pastries can we get you?");
-          Console.WriteLine("(choose an integer greater than or equal to 0)");
-          Console.WriteLine("how many loaves of bread can we get you?");
-          Console.WriteLine("(choose an integer greater than or equal to 0)");
+          List<int> order = new List<int>(takeOrder("en"));
     }
 
+    static List<int> takeOrder(string language)
+    {
+      List<int> order = new List<int>();
+      if (language == "en")
+      {
+        Console.WriteLine("how many pastries can we get you?");
+        Console.WriteLine("(choose an integer greater than or equal to 0)");
+        order.Add(ValidateInteger(Console.ReadLine()));
+        Console.WriteLine("how many loaves of bread can we get you?");
+        Console.WriteLine("(choose an integer greater than or equal to 0)");
+        order.Add(ValidateInteger(Console.ReadLine()));
+        return order;
+      }
+      else
+      {
+        Console.WriteLine("how many pastries can we get you?");
+        Console.WriteLine("(choose an integer greater than or equal to 0)");
+        order.Add(ValidateInteger(Console.ReadLine()));
+        Console.WriteLine("how many loaves of bread can we get you?");
+        Console.WriteLine("(choose an integer greater than or equal to 0)");
+        order.Add(ValidateInteger(Console.ReadLine()));
+        return order;
+      }
+    }
   }
 }
